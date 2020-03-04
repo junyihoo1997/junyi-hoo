8c547746afea6bf461bea2ef625624fe197abb8d
Jun Yi Hoo <junyi_hoo1997@hotmail.com>
2020-03-04 17:09:10 +0800

    Updates

---
+++ precache-manifest.922be1a529f5f616a2eea53a0843faea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b27768683ea345d977e6b31cd257b7e",
+    "url": "/junyihoo/index.html"
+  },
+  {
+    "revision": "ff2c43715c06f6dbf70a",
+    "url": "/junyihoo/static/css/main.b70199cd.chunk.css"
+  },
+  {
+    "revision": "acca071270c2ad86c409",
+    "url": "/junyihoo/static/js/2.97337197.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/junyihoo/static/js/2.97337197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2c43715c06f6dbf70a",
+    "url": "/junyihoo/static/js/main.4bcb6a75.chunk.js"
+  },
+  {
+    "revision": "5f67f6d7b73a10982700",
+    "url": "/junyihoo/static/js/runtime-main.4cfa348c.js"
+  }
+]);
